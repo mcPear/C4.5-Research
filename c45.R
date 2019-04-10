748434898f1613ff01a7224828da2a490d836c88
Maciej Gruszczyński <maciek.gruszka@gmail.com>
2019-04-10 23:33:25 +0200

    classifier in separate file | stratification seems not working

---
+++ c45.R
@@ -0,0 +1,71 @@
+library(caret)
+library(MLmetrics)
+
+c45 <- function(data, folds, C, M, N, Q, R, stratified){ # if R then N else C
+  data <- data[sample(nrow(data)),]
+
+  x_data <- data[,1:ncol(data)-1]
+  y_data <- as.factor(data[,ncol(data)])
+  
+  custom_metric <- function (data, lev = NULL, model = NULL) {
+    accuracy <- Accuracy(data$obs, data$pred)
+    precision <- Precision(data$obs, data$pred)
+    recall <- Recall(data$obs, data$pred)
+    f1 <- F1_Score(data$obs, data$pred)
+    
+    names(accuracy) <- c("accuracy")
+    names(precision) <- c("precision")
+    names(recall) <- c("recall")
+    names(f1) <- c("f1")
+    
+    c(accuracy, precision, recall, f1)
+  } 
+  
+  strIndex <- NULL
+  if(stratified) strIndex <- createFolds(y_data, k = folds, returnTrain = T)
+  
+  tc <- trainControl(
+    index = strIndex,
+    method = 'cv',
+    number = folds,
+    summaryFunction = custom_metric)
+
+  c45 = getModelInfo("J48", FALSE)[[1]]
+  
+  c45$parameters <- data.frame("parameter" = c("C","M","N","Q","R"), 
+                               "class" = c("numeric","numeric","numeric","numeric","boolean"), 
+                               "label" = c("Confidence Threshold","Minimum Instances Per Leaf","Number of folds","Seed","Use reduced error pruning"))
+  
+  c45$grid <- function (x, y, len = NULL, search = "grid") 
+  {
+    return(data.frame(C = C, M =M, N = N, Q = Q, R = R))
+  }
+  
+  c45$fit <- function (x, y, wts, param, lev, last, classProbs, ...) 
+  {
+    dat <- if (is.data.frame(x)) 
+      x
+    else as.data.frame(x)
+    dat$.outcome <- y
+    theDots <- list(...)
+    if (any(names(theDots) == "control")) {
+      theDots$control$C <- param$C
+      theDots$control$M <- param$M
+      theDots$control$N <- param$N
+      theDots$control$Q <- param$Q
+      ctl <- theDots$control
+      theDots$control <- NULL
+    }
+    else {
+      if (param$R) ctl <- RWeka::Weka_control(M = param$M, Q = param$Q, N = param$N, R = param$R)
+      else ctl <- RWeka::Weka_control(C = param$C, M = param$M, Q = param$Q)
+    }
+    modelArgs <- c(list(formula = as.formula(".outcome ~ ."), 
+                        data = dat, control = ctl), theDots)
+    out <- do.call(RWeka::J48, modelArgs)
+    out
+  }
+  
+  model <- train(x = x_data, y = y_data, trControl=tc, method=c45)
+  return(model)
+}
--- notebook.R
+++ notebook.R
@@ -1,6 +1,7 @@
 # load the libraries
 library(caret)
 library(MLmetrics)
+source("/home/maciej/myProjects/decision_tree/c45.R")
 
 # load datasets
 glass <- read.csv("/home/maciej/myProjects/decision_tree/data/glass_data", header = FALSE)
@@ -8,74 +9,9 @@ wine <- read.csv("/home/maciej/myProjects/decision_tree/data/wine_data", header 
 pima <- read.csv("/home/maciej/myProjects/decision_tree/data/pima_data", header = FALSE)
 iris <- read.csv("/home/maciej/myProjects/decision_tree/data/iris_data", header = FALSE)
 
-#choose dataset
-data = wine
-x_data = data[,1:ncol(data)-1]
-y_data = as.factor(data[,ncol(data)])
 
-custom_metric <- function (data, lev = NULL, model = NULL) {
-  accuracy <- Accuracy(data$obs, data$pred)
-  precision <- Precision(data$obs, data$pred)
-  recall <- Recall(data$obs, data$pred)
-  f1 <- F1_Score(data$obs, data$pred)
-
-  names(accuracy) <- c("accuracy")
-  names(precision) <- c("precision")
-  names(recall) <- c("recall")
-  names(f1) <- c("f1")
-  
-  c(accuracy, precision, recall, f1)
-} 
-
-folds <- 10
-cvIndex <- createFolds(y_data, folds, returnTrain = T)
-tc <- trainControl(
-                  # index = cvIndex, #comment to make not stratified (but what with shuffle ?)
-                  method = 'cv',
-                  number = folds,
-                  summaryFunction = custom_metric)
-
-c45 = getModelInfo("J48", FALSE)[[1]]
-
-# defaults by 
-
-c45$parameters <- data.frame("parameter" = c("C","M","N","Q","R"), 
-                             "class" = c("numeric","numeric","numeric","numeric","boolean"), 
-                             "label" = c("Confidence Threshold","Minimum Instances Per Leaf","Number of folds","Seed","Use reduced error pruning"))
-
-c45$grid <- function (x, y, len = NULL, search = "grid") 
-{
-  return(data.frame(C = 0.25, M = 2, N = 2, Q = 1, R = TRUE)) # if R then N else C
+for(folds in c(2,3,5,10)){
+  model <- c45(iris, folds, C = 0.25, M = 2, N = 3, Q = 1, R = FALSE, stratified = TRUE)
+  # print(model$resample)
+  print(model$results$f1)
 }
-
-c45$fit <- function (x, y, wts, param, lev, last, classProbs, ...) 
-{
-  dat <- if (is.data.frame(x)) 
-    x
-  else as.data.frame(x)
-  dat$.outcome <- y
-  theDots <- list(...)
-  if (any(names(theDots) == "control")) {
-    theDots$control$C <- param$C
-    theDots$control$M <- param$M
-    theDots$control$N <- param$N
-    theDots$control$Q <- param$Q
-    ctl <- theDots$control
-    theDots$control <- NULL
-  }
-  else {
-    if (param$R) ctl <- RWeka::Weka_control(M = param$M, Q = param$Q, N = param$N, R = param$R)
-    else ctl <- RWeka::Weka_control(C = param$C, M = param$M, Q = param$Q)
-    }
-  modelArgs <- c(list(formula = as.formula(".outcome ~ ."), 
-                      data = dat, control = ctl), theDots)
-  print(modelArgs)
-  out <- do.call(RWeka::J48, modelArgs)
-  out
-}
-
-model <- train(x = x_data, y = y_data, trControl=tc, method=c45) # resample zawiera wyniki po foldach
-# summarize results
-print(model)
-
-# krosswalidacja - shuffluje, wygląda jakby za każdym razem stratyfikował, wypisz resmple i zbadaj
